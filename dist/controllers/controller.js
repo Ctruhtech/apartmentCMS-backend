d18bdfcf83b583ee74f90e26e0ea3481e391b949
Jitender Singh <jitendersingh@Jitenders-MacBook-Pro.local>
2024-02-01 13:50:44 +0530

    specific addition of color variant

--- dist/controllers/controller.js
+++ dist/controllers/controller.js
@@ -148,6 +148,27 @@ apartmentRouter.post("/models/:id/texture", (req, res) => __awaiter(void 0, void
         return res.status(500).send("Internal server error");
     }
 }));
+// specific addition of color variant
+apartmentRouter.post("/models/:id/color", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const entryId = req.params.id;
+        const newColor = req.body;
+        // Check if an entry with the same id already exists
+        const existingEntry = yield findEntryById(entryId);
+        if (!existingEntry) {
+            return res.status(404).send("Entry not found in the database");
+        }
+        // Add the new ColorVariant to the Element
+        existingEntry.colorVariants.push(newColor);
+        // Update the Element in the database
+        const updatedEntry = yield updateEntry(entryId, existingEntry);
+        return res.status(200).json(updatedEntry);
+    }
+    catch (err) {
+        console.error(err);
+        return res.status(500).send("Internal server error");
+    }
+}));
 // update whole structure 
 apartmentRouter.put("/models/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -178,7 +178,33 @@ apartmentRouter.post("/models/:id/texture", async (req: Request, res: Response) 
       console.error(err);
       return res.status(500).send("Internal server error");
     }
-});  
+});
+
+// specific addition of color variant
+apartmentRouter.post("/models/:id/color", async (req: Request, res: Response) => {
+    try {
+      const entryId = req.params.id;
+      const newColor = req.body;
+  
+      // Check if an entry with the same id already exists
+      const existingEntry = await findEntryById(entryId);
+  
+      if (!existingEntry) {
+        return res.status(404).send("Entry not found in the database");
+      }
+  
+      // Add the new ColorVariant to the Element
+      existingEntry.colorVariants.push(newColor);
+  
+      // Update the Element in the database
+      const updatedEntry = await updateEntry(entryId, existingEntry);
+  
+      return res.status(200).json(updatedEntry);
+    } catch (err) {
+      console.error(err);
+      return res.status(500).send("Internal server error");
+    }
+});
 
   
 // update whole structure 
